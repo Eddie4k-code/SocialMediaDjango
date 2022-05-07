109e82b8638ab2af8b37ea9d5fd7efde03f8dda4
Eddie4k-code <98666468+Eddie4k-code@users.noreply.github.com>
2022-05-07 11:34:48 -0400

    Update urls.py

--- TwitterClone/TwitterClone/urls.py
+++ TwitterClone/TwitterClone/urls.py
@@ -20,6 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('', include('django.contrib.auth.urls')),
-    path("unicorn/", include("django_unicorn.urls"))
 
 ]
